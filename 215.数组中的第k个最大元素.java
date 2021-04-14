c608fee5e707d5fe800fd608d52b671f56abf20c
boss_guo <1148636359g@gmail.com>
2021-04-14 12:35:39 +0800

    堆排序算法和快速排序的算法。快速排序算法中不同的partition算法。

--- 215.数组中的第k个最大元素.java
+++ 215.数组中的第k个最大元素.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
+import java.util.Random;
 
 /*
  * @Author: guo yawen
  * @Date: 2021-04-11 17:52:31
- * @LastEditTime: 2021-04-14 00:36:50
+ * @LastEditTime: 2021-04-14 12:33:14
  * @LastEditors: guo yawen
  * @Description: 
  * @FilePath: \LeetCode\215.数组中的第k个最大元素.java
@@ -17,9 +18,11 @@ import java.util.Arrays;
 
 // @lc code=start
 class Solution {
-    public int quickSort(int[] nums, int left, int right, int k){
-        
-        return 0;
+    /*==================heapSort method===================*/
+    public void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
     public void maxHeapify(int[] nums, int i, int size){
         //非递归形式
@@ -54,7 +57,7 @@ class Solution {
             maxHeapify(nums, i, nums.length);
         }
     }
-    public int findKthLargest(int[] nums, int k) {
+    public int findKthLargestHeapSort(int[] nums, int k) {
         if(nums.length == 1){
             return nums[0];
         }
@@ -72,11 +75,61 @@ class Solution {
         System.out.println(Arrays.toString(nums));
         return nums[index];
     }
-    public void swap(int[] nums, int a, int b){
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
+    /*==================heapSort method===================*/
+    
+
+    /*==================quickSort method===================*/
+    //*一种partition算法
+    public int partition(int[] nums,int left, int right){
+        int pivot = nums[left];
+        while(left < right){
+            while(left < right && nums[right] >= pivot) right--;
+            nums[left] = nums[right];
+            while(left < right && nums[left] <= pivot) left++;
+            nums[right] = nums[left];
+        }
+        nums[left] = pivot;
+        return left;
     }
+    //*第二种partition算法
+    public int partition2(int[] nums, int left, int right){
+        int pivot = nums[right];
+        int lefti = left - 1;
+        while(left < right){
+            if(nums[left] < pivot){
+                ++lefti;
+                swap(nums, lefti, left);
+            }
+            left++;
+        }
+        swap(nums, ++lefti, right);
+        return lefti;
+    }
+    //*第三种partition算法，随机化版本 效率最高。时间击败 91%,空间击败 99.74%
+    //!算法导论证明。
+    public int randomizedPartition(int[] nums, int left, int right){
+        Random random = new Random();
+        int i = random.nextInt((right-left)+1)+left;
+        swap(nums, i, right);
+        return partition2(nums, left, right);
+
+    }
+    public int quickSort(int[] nums, int left, int right, int k){
+            int mid = randomizedPartition(nums, left, right);
+            if(mid == k){
+                return nums[k];
+            }else if(mid > k){
+                return quickSort(nums, left, mid-1, k);
+            }else {
+                return quickSort(nums, mid+1, right, k);
+            }
+    }
+    public int findKthLargest(int[] nums, int k) {
+        int index = nums.length - k;
+        return quickSort(nums, 0, nums.length -1, index);
+    }
+
+    /*==================quickSort method===================*/
 }
 // @lc code=end
 
