8f67ddf491c713d33f7f4e98750c493187d0b707
boss_guo <1148636359g@gmail.com>
2021-06-19 23:09:25 +0800

    6.18写，忘记提交，操！

---
+++ 203.移除链表元素.java
@@ -0,0 +1,42 @@
+/*
+ * @author: guo yawen
+ * @Date: 2021-06-18 22:34:59
+ * @lastEditTime: do not edit
+ * @lastEditors: guo yawen
+ * @description: 
+ * @FilePath: \LeetCoded:\javawork\leetCode\203.移除链表元素.java
+ * TrafalgarSX
+ */
+/*
+ * @lc app=leetcode.cn id=203 lang=java
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode vHead = new ListNode(-1, head);
+        ListNode curr = vHead;
+        while( curr != null && curr.next != null){
+            if(curr.next.val == val){
+                curr.next = curr.next.next;
+            }else{
+                curr = curr.next;
+            }
+        }
+        return vHead.next;
+    }
+}
+// @lc code=end
+
